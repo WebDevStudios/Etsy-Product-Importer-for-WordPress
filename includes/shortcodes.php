fcf70725f901f0235a38b675a1285d5cff35006f
Michael Beckwith <tw2113@gmail.com>
2016-06-10 01:29:22 -0500

    so many comments

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+ * Register our shortcodes.
+ */
 class Etsy_Importer_Shortcodes {
 
+	/**
+	 * Put everything together.
+	 */
 	public function __construct() {
-		// Add shortcodes
+		// Add shortcodes.
 		add_shortcode( 'product_link', array( $this, 'product_link_shortcode' ) );
 		add_shortcode( 'product_content', array( $this, 'product_content_shortcode' ) );
 		add_shortcode( 'product_images', array( $this, 'product_images_shortcode' ) );
@@ -14,29 +20,28 @@ class Etsy_Importer_Shortcodes {
 	 *
 	 * @since 1.0
 	 *
-	 * @param array       $atts Array of shortcode attributes.
-	 * @param string|null $content
-	 *
+	 * @param array       $atts    Array of shortcode attributes.
+	 * @param string|null $content Shortcode content.
 	 * @return string $output
 	 */
 	public function product_link_shortcode( $atts, $content = null ) {
 
-		// Get our shortcode attributes
+		// Get our shortcode attributes.
 		$atts = shortcode_atts( array(
 			'id'		=> '',
 			'external'	=> '',
 			'title'		=> '',
 		), $atts, 'product_link' );
 
-		// Get our post content
+		// Get our post content.
 		$product = get_post( $atts['id'] );
 
-		// If there is no product found, stop
+		// If there is no product found, stop.
 		if ( ! $product ) {
 			return;
 		}
 
-		// Get our post or external link
+		// Get our post or external link.
 		if ( 'yes' == $atts['external'] || 'true' == $atts['external'] ) {
 
 			$link 	= esc_url( get_post_meta( $atts['id'], '_etsy_product_url', true ) );
@@ -48,13 +53,13 @@ class Etsy_Importer_Shortcodes {
 			$target	= '_self';
 		}
 
-		// Get our link title
+		// Get our link title.
 		$title = ( $atts['title'] ) ? $atts['title'] : get_the_title( $atts['id'] );
 
-		// Assume zer is nussing
+		// Assume zer is nussing.
 		$output = '';
 
-		// If our title and link return something, display the link
+		// If our title and link return something, display the link.
 		if ( $title && $link ) {
 			$output .= '<p><a href="' . $link . '" title="' . $title . '" target="' . $target . '">' . $title . '</a></p>';
 		}
@@ -67,36 +72,35 @@ class Etsy_Importer_Shortcodes {
 	 *
 	 * @since 1.0
 	 *
-	 * @param array       $atts Array of shortcode attributes.
-	 * @param string|null $content
-	 *
+	 * @param array       $atts    Array of shortcode attributes.
+	 * @param string|null $content Shortcode content.
 	 * @return string
 	 */
 	public function product_content_shortcode( $atts, $content = null ) {
 
-		// Get our shortcode attributes
+		// Get our shortcode attributes.
 		$atts = shortcode_atts( array(
 			'id'		=> '',
 			'length'	=> '',
 		), $atts, 'product_content' );
 
-		// Get our post content
+		// Get our post content.
 		$product = get_post( $atts['id'] );
 
-		// If there is no product found, stop
+		// If there is no product found, stop.
 		if ( ! $product ) {
 			return;
 		}
 
 		$content = wpautop( $product->post_content );
 
-		// Assume zer is nussing
+		// Assume zer is nussing.
 		$output = '';
 
-		// If our content returns something, display it
+		// If our content returns something, display it.
 		if ( $content ) {
 
-			// If we have a length set, apply it
+			// If we have a length set, apply it.
 			if ( '' !== $atts['length'] ) {
 
 				$excerpt_length = $atts['length'];
@@ -118,23 +122,22 @@ class Etsy_Importer_Shortcodes {
 	 *
 	 * @since 1.0
 	 *
-	 * @param array       $atts Array of shortcode attributes.
-	 * @param string|null $content
-	 *
+	 * @param array       $atts    Array of shortcode attributes.
+	 * @param string|null $content Shortcode content.
 	 * @return string
 	 */
 	public function product_images_shortcode( $atts, $content = null ) {
 
-		// Get our shortcode attributes
+		// Get our shortcode attributes.
 		$atts = shortcode_atts( array(
 			'id'	=> '',
 			'size'	=> 'thumbnail',
 		), $atts, 'product_images' );
 
-		// Get our post content
+		// Get our post content.
 		$product = get_post( $atts['id'] );
 
-		// If there is no product found, stop
+		// If there is no product found, stop.
 		if ( ! $product ) {
 			return;
 		}
@@ -145,27 +148,27 @@ class Etsy_Importer_Shortcodes {
 			'post_parent'		=> $atts['id'],
 		), $atts );
 
-		// Get our post images
+		// Get our post images.
 		$images = get_posts( $img_args );
 
 		$thumb_size = apply_filters( 'etsy_importer_product_images_shortcode_thumb_size', $atts['size'], $atts );
 
-		// Assume zer is nussing
+		// Assume zer is nussing.
 		$output = '';
 
-		// If our content returns something, display it
+		// If our content returns something, display it.
 		if ( $images ) {
 
 			foreach ( $images as $image ) {
 
-				// Set the image ID
+				// Set the image ID.
 				$image_id = $image->ID;
 
-				// Grab the image based on the size passed in the shortcode
+				// Grab the image based on the size passed in the shortcode.
 				$image_thumb 	= wp_get_attachment_image( $image_id, $thumb_size );
 				$image_full 	= wp_get_attachment_image_src( $image_id, 'full' );
 
-				// Display the image
+				// Display the image.
 				$output .= '<a href="' . $image_full[0]. '" class="thickbox" rel="gallery-' . $atts['id'] . '">' . $image_thumb . '</a>';
 
 			}
